b73ebee66fde6c56cf28105bed0713fb1496b047
8gaU8 <--set>
2025-11-11 02:02:52 +0900

    add devtools setting

---
+++ 00-devtools-settings.zsh
@@ -0,0 +1,8 @@
+# ghcup
+source ${HOME}/.ghcup/env
+
+# Added by LM Studio CLI (lms)
+export PATH="$PATH:/Users/hagayuya/.lmstudio/bin"
+# End of LM Studio CLI section
+
+eval "$(/Users/hagayuya/.local/bin/mise activate zsh)"
