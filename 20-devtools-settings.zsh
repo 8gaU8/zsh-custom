f2a343e7f64fb5d6e88b00af346b70f86e9314f6
8gaU8 <--set>
2025-11-16 04:31:11 +0900

    tidy

--- 20-devtools-settings.zsh
+++ 20-devtools-settings.zsh
@@ -1,16 +1,36 @@
 source "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/10-utils.zsh"
 
-# update path
-# export PATH="${HOME}/.local/bin:$PATH"
+#* === update path ===
+
+## Add common user bin directories
 add_to_path_if_exists "${HOME}/.local/bin"
+## LM Studio
+add_to_path_if_exists "${HOME}/.lmstudio/bin"
 
 
-# ghcup
+
+#* === Environment Variables ===
+# pip: Avoid pip installation without venv
+export PIP_REQUIRE_VIRTUALENV=1
+
+# man: use bat as manpager
+export MANPAGER="sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | bat -p -lman'"
+
+
+
+#* === Activations ===
+## Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+## Mise
+eval "$(${HOME}/.local/bin/mise activate zsh)"
+## ghcup
 source_if_exists ${HOME}/.ghcup/env
 
-# Added by LM Studio CLI (lms)
-add_to_path_if_exists "${HOME}/.lmstudio/bin"
-# End of LM Studio CLI section
+#* === Completions ===
 
-
-eval "$(${HOME}/.local/bin/mise activate zsh)"
+## uv
+if command -v uv &> /dev/null; then
+  eval "$(uv generate-shell-completion zsh)"
+else
+  echo "uv command not found, skipping uv autocompletion setup."
+fi
--- 30-autocompletions.zsh
+++
@@ -1,5 +0,0 @@
-if command -v uv &> /dev/null; then
-  eval "$(uv generate-shell-completion zsh)"
-else
-  echo "uv command not found, skipping uv autocompletion setup."
-fi
