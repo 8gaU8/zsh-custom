3c1bcf0ae448a9596f09a67471f7d0ae5fff14e6
8gaU8 <--set>
2025-11-16 03:44:50 +0900

    use env for path

--- 20-devtools-settings.zsh
+++ 20-devtools-settings.zsh
@@ -1,11 +1,12 @@
 # update path
-export PATH="/Users/hagayuya/.local/bin:$PATH"
+export PATH="${HOME}/.local/bin:$PATH"
+
 
 # ghcup
 source ${HOME}/.ghcup/env
 
 # Added by LM Studio CLI (lms)
-export PATH="$PATH:/Users/hagayuya/.lmstudio/bin"
+export PATH="$PATH:${HOME}/.lmstudio/bin"
 # End of LM Studio CLI section
 
-eval "$(/Users/hagayuya/.local/bin/mise activate zsh)"
+eval "$(${HOME}/.local/bin/mise activate zsh)"
